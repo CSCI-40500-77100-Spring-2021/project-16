dc3d7262d33c541348cf3173d73bf0f56c1ffa35
CalvinH1 <37669955+CalvinH1@users.noreply.github.com>
2021-04-25 11:04:04 -0400

    Update Homepage.js
    Changed it so it can use a function to display all the row therefore we can useEffect to pull data from the backend to display tournaments

--- budokai/src/Homepage.js
+++ budokai/src/Homepage.js
@@ -1,22 +1,80 @@
-import {Container,Row,Col, Nav} from 'react-bootstrap'
+import {Container,Row} from 'react-bootstrap'
 import Tournamentbox from './Tournamentbox';
+import React, { useState,useEffect } from 'react';
 
+function createlist(doc){
+    var items = []
+    for(let i= 0; i<doc.length;i+=3)
+    {
+        if(i+2 < doc.length)
+        {
+            items.push(
+            <div>
+            <br/>
+            <Row>
+            <Tournamentbox game = {doc[i].game} name= {doc[i].name} date= {doc[i].date} time={doc[i].time} likes= {doc[i].likes}/>
+            <Tournamentbox game = {doc[i+1].game} name= {doc[i+1].name} date= {doc[i+1].date} time={doc[i+1].time} likes= {doc[i+1].likes}/>
+            <Tournamentbox game = {doc[i+2].game} name= {doc[i+2].name} date= {doc[i+2].date} time={doc[i+2].time} likes= {doc[i+2].likes}/>
+            </Row>
+            </div>
+            );
+        }
+        else if(i+1 < doc.length)
+        {
+            items.push(
+            <div>
+            <br/>
+            <Row>
+            <Tournamentbox game = {doc[i].game} name= {doc[i].name} date= {doc[i].date} time={doc[i].time} likes= {doc[i].likes}/>
+            <Tournamentbox game = {doc[i+1].game} name= {doc[i+1].name} date= {doc[i+1].date} time={doc[i+1].time} likes= {doc[i+1].likes}/>
+            </Row>
+            </div> 
+            );
+        }
+        else if(i < doc.length)
+        {
+            items.push(
+            <div>
+            <br/>
+            <Row>
+            <Tournamentbox game = {doc[i].game} name= {doc[i].name} date= {doc[i].date} time={doc[i].time} likes= {doc[i].likes}/>
+            </Row>
+            </div> 
+            );
+        }
+    }
+    console.log(items)
+    return items
+}
 function Homepage()
 {
+    const [json_array, setJSON] = useState([])
+    
+    
+    const doc = [
+            {
+                game: "SpringK",
+                name: "SpringK 2021",
+                date:"Fri, Apr 2nd",
+                time: "9PM CET" ,
+                likes: "24"
+            },
+            {
+                game :"RocketL", name: "Rocket League Local", date: "Tue, Apr 6th",time: "6PM EST", likes: "33"
+            }
+            ,
+            {
+                game :"Chess", name: "Minecraft Creative Build Tournament" ,date: "Sat, Apr 11th",time: "9PM EST", likes: "16"
+            },
+            {
+                game :"DTN" ,name: "Defend the North", date: "Sun, Apr 4th",time: "9PM EST" ,likes: "122"
+            }
+        ]
+    
+
     return(
         <Container>
-            <br/>
-            <Row>
-                <Tournamentbox game ="SpringK" name= "SpringK 2021" date= "Fri, Apr 2nd"time= "9PM CET" likes= "24"/>
-                <Tournamentbox game ="RocketL" name= "Rocket League Local" date= "Tue, Apr 6th"time= "6PM EST" likes= "33"/>
-                <Tournamentbox game ="Chess" name= "Minecraft Creative Build Tournament" date= "Sat, Apr 11th"time= "9PM EST" likes= "16"/>
-            </Row>
-            <br/>
-            <Row>
-                <Tournamentbox game ="DTN" name= "Defend the North" date= "Sun, Apr 4th"time= "9PM EST" likes= "122"/>
-                <Tournamentbox game ="Minecraft" name= "Chess tournament" date= "Mon, Apr 5th"time= "5PM EST" likes= "10"/>
-                <Tournamentbox game ="LMBM" name= "Let's Make Big Moves" date= "Sun, Apr 4th"time= "5PM EST" likes= "45"/>
-            </Row>
+            {createlist(doc)}
         </Container>
     );
 }
