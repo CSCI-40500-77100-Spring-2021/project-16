baa8fefd2821c435dea8dbcd3a82ac715d331fae
CalvinH1 <37669955+CalvinH1@users.noreply.github.com>
2021-05-10 00:19:47 -0400

    Update Tournamentpage.js

--- budokai/src/Tournamentpage.js
+++ budokai/src/Tournamentpage.js
@@ -7,6 +7,11 @@ import Minecraft from './img/Minecraft.png'
 import LMBM from './img/LMBM.jpg'
 import Smash from './img/smash.jpg'
 import './css/Tournamentpage.css'
+import React, { useState,useEffect } from 'react';
+import {useParams} from 'react-router-dom'
+
+import axios from 'axios';
+
 let images = new Map()
 images.set("Spring Kick 2021",SpringK)
 images.set("Rocket League",RocketL)
@@ -17,40 +22,57 @@ images.set("Let's Make Big Moves",LMBM)
 images.set("Smash",Smash)
 function Tournamentpage()
 {
+    const [json_array, setJSON] = useState({})
+    let {id} = useParams();
+    useEffect(()=>
+    {
+        axios.get(`https://budokai.karmazin.me/events/get_event`,{
+            params: {
+                id: id
+              }
+        })
+            .then(res =>{
+                var x = JSON.parse(res.data)
+                console.log(x)
+                setJSON(x.Item)
+            })
+    },[])
+
     return(
     <div className = "body-wrapper">
-        <img src={SpringK} alt="Spring Kick" className ="hero-img"/>
+        <img src={images.get(json_array.game)} alt="Spring Kick" className ="hero-img"/>
         <div class="eventSection" id="upcoming">
-        <h4>Spring Kick 2021</h4>
+        <h4>{json_array.game}</h4>
         </div>
         <Container> 
             <Row>
                 <Col lg ={4} md ={4} sm={4}xs={12}>
                     <h5>How to register</h5>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id porta lectus. In pretium nulla id eros imperdiet molestie. Quisque tempus consequat arcu, a iaculis ex ornare ac. Nullam ac enim egestas nulla auctor fermentum a non arcu. Nunc in imperdiet sapien. Etiam arcu magna, consequat eget magna fermentum, porttitor varius velit. Sed elementum aliquet ex ultrices hendrerit. Nullam vitae sem sapien. </p>
+                    <p>{json_array.registration}</p>
                 </Col>
                 <Col lg={2} md ={2} sm={2} xs={12}>
                     <h5>Date</h5>
-                    <p>Fri, Apr 2nd</p>
+                    <p>{json_array.eventDate}</p>
                     <h5>Time</h5>
-                    <p>6PM EST</p>
+                    <p>{json_array.time}</p>
                     <h5>Entry Price</h5>
-                    <p>Free</p>
+                    <p>{json_array.price}</p>
                     <h5>Prize</h5>
-                    <p>$5000</p>
-                    <h5>Region</h5>
-                    <p>US</p>
+                    <p>{json_array.prize}</p>
+                    <h5>Address</h5>
+                    <p>{json_array.address}</p>
                     <h5>Type</h5>
-                    <p>Online</p>
+                    <p>{json_array.type}</p>
                     <h5>Stream</h5>
-                    <a href="#">Click me!</a>
+                    <a href={json_array.stream}>Click me!</a>
                 </Col>
                 <Col lg ={6} md ={6} sm={6} xs={12}>
                     <h5>Description</h5>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id porta lectus. In pretium nulla id eros imperdiet molestie. Quisque tempus consequat arcu, a iaculis ex ornare ac. Nullam ac enim egestas nulla auctor fermentum a non arcu. Nunc in imperdiet sapien. Etiam arcu magna, consequat eget magna fermentum, porttitor varius velit. Sed elementum aliquet ex ultrices hendrerit. Nullam vitae sem sapien. Sed vel dictum dui. Duis suscipit porttitor molestie. Duis tortor nisl, bibendum eget elit id, mollis mollis nulla. Nullam dapibus, nisl sed lacinia lobortis, leo nunc tincidunt neque, a commodo lectus tortor vel erat. Maecenas porta dui ut felis molestie, in commodo sem convallis.</p>
+                    <p>{json_array.description}</p>
                     <h5>Registration process</h5>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id porta lectus. In pretium nulla id eros imperdiet molestie. Quisque tempus consequat arcu, a iaculis ex ornare ac. Nullam ac enim egestas nulla auctor fermentum a non arcu. Nunc in imperdiet sapien. Etiam arcu magna, consequat eget magna fermentum, porttitor varius velit. Sed elementum aliquet ex ultrices hendrerit. Nullam vitae sem sapien. Sed vel dictum dui. Duis suscipit porttitor molestie. Duis tortor nisl, bibendum eget elit id, mollis mollis nulla. Nullam dapibus, nisl sed lacinia lobortis, leo nunc tincidunt neque, a commodo lectus tortor vel erat. Maecenas porta dui ut felis molestie, in commodo sem convallis.</p>
+                    <p>{json_array.registration}</p>
                 </Col>
+            
             </Row>
         </Container>
     </div>
