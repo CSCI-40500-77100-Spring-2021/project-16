562e7935cd64ccee974caf0d169af11a1128f506
CalvinH1 <37669955+CalvinH1@users.noreply.github.com>
2021-05-21 13:23:09 -0400

    Add files via upload
    Added two files for testing

---
+++ budokai/src/Form.test.js
@@ -0,0 +1,142 @@
+import React from "react";
+import Enzyme, {shallow,mount } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+import Form from './Form'
+
+
+
+const validate = (name,price,prize,register,description,addressp,streamp)=>{
+    const stream = /https:\/\/www\.twitch\.tv\/(?!.+\/profile).*/
+    const money = /^-?(?:0|[1-9]\d{0,2}(?:,?\d{3})*)(?:\.\d+)?$/
+    const address =/^[a-zA-Z0-9\s\,\''\-]*$/
+    let valid = true 
+    if(!name)
+    {
+        valid = false
+    }
+    if(!money.test(price))
+    {
+        valid = false
+    }
+    if(!money.test(prize))
+    {
+        valid = false
+    }
+    if(!register)
+    {
+        valid = false
+    }
+    if(!description)
+    {
+        valid = false
+    }
+    if(!address.test(addressp))
+    {
+        valid = false
+    }
+    if(!stream.test(streamp))
+    {
+        valid = false
+    }
+    return valid 
+  }
+  test('validate form information with no name', ()=>
+  {
+      expect(validate("",
+      "100",
+      "0",
+      "Lorem",
+      "Ipsum"
+      ,"695 Park Ave, New York, NY 10065"
+      ,"https://www.twitch.tv/loltyler1")).toBe(false)
+    
+  })
+  test('validate form information with no information', ()=>
+  {
+      expect(validate("",
+      "",
+      "",
+      ""
+      ,""
+      ,""
+      ,"")).toBe(false)
+    
+  })
+  test('validate form information', ()=>
+  {
+      expect(validate("Chess",
+      "100",
+      "0",
+      "Lorem"
+      ,"Ipsum"
+      ,"695 Park Ave, New York, NY 10065"
+      ,"https://www.twitch.tv/loltyler1")).toBe(true)
+    
+  })
+  test('validate form information with wrong cash', ()=>
+  {
+      expect(validate("Chess",
+      "100",
+      "0A",
+      "Lorem"
+      ,"Ipsum"
+      ,"695 Park Ave, New York, NY 10065"
+      ,"https://www.twitch.tv/loltyler1")).toBe(false)
+    
+  })
+  test('validate form information with cash as not a number', ()=>
+  {
+      expect(validate("Chess",
+      "ABCD",
+      "0A",
+      "Lorem"
+      ,"Ipsum"
+      ,"695 Park Ave, New York, NY 10065"
+      ,"https://www.twitch.tv/loltyler1")).toBe(false)
+    
+  })
+  test('validate form information with no description ', ()=>
+  {
+      expect(validate("Chess",
+      "100",
+      "0",
+      "Ipsum"
+      ,""
+      ,"695 Park Ave, New York, NY 10065"
+      ,"https://www.twitch.tv/loltyler1")).toBe(false)
+    
+  })
+  test('validate form information with wrong address', ()=>
+  {
+      expect(validate("Chess",
+      "100",
+      "0",
+      "Lorem",
+      "Ipsum"
+      ,"ABC 123"
+      ,"https://www.twitch.tv/loltyler1")).toBe(false)
+    
+  })
+  test('validate form information with wrong link', ()=>
+  {
+      expect(validate("Chess",
+      "100",
+      "0",
+      "Lorem",
+      "Ipsum"
+      ,"695 Park Ave, New York, NY 10065"
+      ,"https://www.youtube.com/watch?v=2d-SX8YRyis")).toBe(false)
+    
+  })
+  test('validate form information with no Register info', ()=>
+  {
+      expect(validate("Chess",
+      "100",
+      "0",
+      ""
+      ,"Ipsum"
+      ,"695 Park Ave, New York, NY 10065"
+      ,"https://www.twitch.tv/loltyler1")).toBe(false)
+    
+  })
---
+++ budokai/src/Login.test.js
@@ -0,0 +1,42 @@
+const validate = (emailp,pass)=>{
+    const email = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+    const password = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/
+    let valid = true
+    if(!email.test(emailp))
+    {
+        valid = false
+    }
+    if(!password.test(pass))
+    {
+        valid = false
+    }
+    return valid
+}
+test("validate login",()=>{
+    expect(validate("test@gmail.com","Abcdef888#")).toBe(true)
+}
+)
+test("validate login with empty email",()=>{
+    expect(validate("","Abcdef888#")).toBe(false)
+}
+)
+test("validate login with no email",()=>{
+    expect(validate("HolaComoEsta","Abcdef888#")).toBe(false)
+}
+)
+test("validate login with invalid password",()=>{
+    expect(validate("test@gmail.com","abc")).toBe(false)
+}
+)
+test("validate login with no password",()=>{
+    expect(validate("test@gmail.com","")).toBe(false)
+}
+)
+test("validate login with invalid password password",()=>{
+    expect(validate("test@gmail.com","abcdefg12)(")).toBe(false)
+}
+)
+test("validate login with invalid password password",()=>{
+    expect(validate("test@gmail.com","Abcdef!()123")).toBe(false)
+}
+)
